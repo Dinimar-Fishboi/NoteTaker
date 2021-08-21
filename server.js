029ad43236cc8aa910a7af0a019e4e491585ff9d
Dinimar Fish <tayla@tallisman.com.au>
2021-08-21 12:00:24 +1000

    fixed syntax error

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} 🪶`)
 );
