f7903dfac706452b4218c7aa0bafe283c3b79a35
Dinimar Fish <tayla@tallisman.com.au>
2021-08-21 16:03:32 +1000

    resolved note POST - new notes now added to list and db.json file by user

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const noteRouter = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-//const { v4: uuidv4 } = require('uuid');
+const uuid = require('../helpers/uuid');
 
 //This should just get all the pre-existing data to upload
 noteRouter.get('/', (req, res) => {
@@ -11,15 +11,34 @@ noteRouter.get('/', (req, res) => {
 
   noteRouter.post('/', (req, res) => {
     console.info(`${req.method} request received to submit a note`);
+
+    const { title, text} = req.body;
+
+    if (title && text){
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    }
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    }
+
+      res.json(response);
+    } else {
+      res.json('Error: cannot post new note')
+    }
     
     // THIS IS A READ AND APPEND SITUATION
-    //readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
   noteRouter.delete('/', (req, res) => {
     console.info(`${req.method} request received to delete note`);
   
-   // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
   module.exports = noteRouter;
