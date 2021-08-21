c47f1fe5558165b70ce861b38c165a3f8381b51e
Dinimar Fish <tayla@tallisman.com.au>
2021-08-21 10:51:23 +1000

    added initial noteRouter get, post and delete objects and exported notes.js to index.js

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,22 @@ const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 noteRouter.get('/', (req, res) => {
-    console.info(`${req.method} request received for feedback`);
+    console.info(`${req.method} request received for notes`);
   
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+  noteRouter.post('/', (req, res) => {
+    console.info(`${req.method} request received to submit a note`);
+    
+    // THIS IS A READ AND APPEND SITUATION
+    //readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+  noteRouter.delete('/', (req, res) => {
+    console.info(`${req.method} request received to delete note`);
+  
+   // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+  module.exports = noteRouter;
